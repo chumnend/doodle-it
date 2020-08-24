ff7118f1f08cc7defa7d1ae9f00a7b11265d9b66
Nicholas Chumney <nchumney@hotmail.com>
2020-08-24 16:40:19 -0400

    moved Switch to seperate component Router

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
 import React, { useState } from 'react';
-import { Switch, Route, Redirect } from "react-router-dom";
+import Router from './Router';
 import "./App.scss";
 
-import Auth from "./pages/Auth";
-import Console from "./pages/Console";
-import Editor from "./pages/Editor";
-import NotFound from "./pages/NotFound";
-
 function App() {
-    const [loggedIn, hasLoggedIn] = useState(false);
-    const [user, setUser] = useState({});
+  const [loggedIn, hasLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
 
-    const appProps = { loggedIn, hasLoggedIn, user, setUser };
-    return (
-        <div className="App">
-            <Switch>
-                <Route 
-                    path="/auth" 
-                    render={ props => 
-                        <Auth {...props} {...appProps} /> 
-                    }
-                />
-                <Route 
-                    path="/editor" 
-                    render={ props => 
-                        loggedIn
-                            ? <Editor {...props} {...appProps} />
-                            : <Redirect to="/auth" />
-                    }
-                />
-                <Route 
-                    path="/console" 
-                    render={ props => 
-                        loggedIn
-                            ? <Console {...props} {...appProps} />
-                            : <Redirect to="/auth" />
-                    }
-                />
-                <Route 
-                    path="/" 
-                    render={ props => 
-                        loggedIn
-                            ? <Redirect to="/console" />
-                            : <Redirect to="/auth" />
-                    }
-                />
-                <Route component={NotFound} />
-            </Switch>
-        </div>
-    );
+  const appProps = { loggedIn, hasLoggedIn, user, setUser };
+  return (
+    <div className="App">
+      <Router appProps={ appProps } />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Router.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import Auth from "./pages/Auth";
+import Console from "./pages/Console";
+import Editor from "./pages/Editor";
+import NotFound from "./pages/NotFound";
+
+function Router({ appProps }) {
+const { loggedIn } = appProps;
+
+  return (
+    <Switch>
+      <Route 
+        path="/auth" 
+        render={ props => 
+          <Auth {...props} {...appProps} /> 
+        }
+      />
+      <Route 
+        path="/editor" 
+        render={ props => 
+          loggedIn
+            ? <Editor {...props} {...appProps} />
+            : <Redirect to="/auth" />
+        }
+      />
+      <Route 
+        path="/console" 
+        render={ props => 
+          loggedIn
+            ? <Console {...props} {...appProps} />
+            : <Redirect to="/auth" />
+        }
+      />
+      <Route 
+        path="/" 
+        render={ props => 
+          loggedIn
+            ? <Redirect to="/console" />
+            : <Redirect to="/auth" />
+        }
+      />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+export default Router;
