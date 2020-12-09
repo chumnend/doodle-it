5b2e258230dad21b36dbb37c775337ef2d804d29
Nicholas Chumney <nicholas.chumney@outlook.com>
2020-12-09 18:07:27 -0500

    added test utilities

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,8 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { configureStore } from './store';
-import { render } from '@testing-library/react';
+import { render } from './utils/test-utils';
 import App from './App';
-
-const store = configureStore();
 
 describe('<App>', () => {
   it('renders without crashing', () => {
-    render(
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>,
-    );
+    render(<App />);
   });
 });
