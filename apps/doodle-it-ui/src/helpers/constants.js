bf219145d5bd033d9409a102c57559f5bc6811d6
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-01-23 20:12:43 -0500

    add react redux and react router

---
+++ apps/doodle-it-ui/src/helpers/constants.js
@@ -0,0 +1,8 @@
+export const LANDING = '/';
+export const HOME = '/home';
+export const DESIGNER = '/design/:id?';
+export const DESIGNER_NEW = '/design';
+export const DESIGNER_WITH_ID = (id) => `/design/${id}`;
+export const REGISTER = '/register';
+export const LOGIN = '/login';
+export const LOGOUT = '/logout';
---
+++ apps/doodle-it-ui/src/helpers/theme.js
@@ -0,0 +1,24 @@
+export const color = {
+  red: '#ff0033',
+  blue: '#2890bd',
+  black: '#000',
+  ebony: '#383838',
+  darkgrey: '#6e6e6e',
+  grey: '#c9c9c9',
+  white: '#f2f2f2',
+};
+
+export const device = {
+  xs: '320px',
+  sm: '575px',
+  md: '768px',
+  lg: '991px',
+  xl: '1199',
+};
+
+export const layout = {
+  navHeight: '64px',
+  toolbarHeight: '48px',
+  buttonSize: '50px',
+  cardSize: '200px',
+};
