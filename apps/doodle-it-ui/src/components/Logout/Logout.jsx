5f39a36c6b98c6738553407d7dd0680f6c70471c
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-01-27 15:30:55 -0500

    rebuild Logout component

--- apps/doodle-it-ui/src/components/Logout/Logout.jsx
+++ apps/doodle-it-ui/src/components/Logout/Logout.jsx
@@ -1,9 +1,25 @@
-function Logout() {
-  return (
-    <>
-      <h1>Logout</h1>
-    </>
-  )
-}
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+//import { useDispatch } from 'react-redux';
 
-export default Logout
+import { path } from '../../helpers/constants';
+// import * as actions from '../../store/actions';
+
+const Logout = () => {
+  // const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // const logoutUser = useCallback(
+  //   () => dispatch(actions.authLogoutRequest()),
+  //   [dispatch],
+  // );
+
+  useEffect(() => {
+    // logoutUser();
+    navigate(path.landing);
+  }, [navigate]);
+
+  return <></>
+};
+
+export default Logout;
