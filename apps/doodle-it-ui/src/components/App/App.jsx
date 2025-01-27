7dd5a7913a831ae8c4eaf448871bb6481e872bf9
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-01-27 14:12:27 -0500

    rebuild Navigation component

--- apps/doodle-it-ui/src/components/App/App.jsx
+++ apps/doodle-it-ui/src/components/App/App.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 
+import Navigation from './components/Navigation';
 import Landing from '../Landing';
 import Designer from '../Designer';
 import Home from '../Home';
@@ -11,16 +13,27 @@ import NotFound from '../NotFound';
 import { path } from '../../helpers/constants';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
   return (
-    <Routes>
-      <Route path={path.home} element={< Home />} />
-      <Route path={path.designer} element={<Designer />} />
-      <Route path={path.register} element={<Register />} />
-      <Route path={path.login} element={<Login />} />
-      <Route path={path.logout} element={<Logout />} />
-      <Route index element={<Landing />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Navigation isLoggedIn={true} />
+      {!isLoading && (
+          <Routes>
+            <Route path={path.home} element={< Home />} />
+            <Route path={path.designer} element={<Designer />} />
+            <Route path={path.register} element={<Register />} />
+            <Route path={path.login} element={<Login />} />
+            <Route path={path.logout} element={<Logout />} />
+            <Route index element={<Landing />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+      )}
+    </>
   );
 };
 
--- apps/doodle-it-ui/src/components/Landing/components/HeroTitle/HeroTitle.jsx
+++ apps/doodle-it-ui/src/components/Landing/components/HeroTitle/HeroTitle.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+
 import * as Styles from './styles';
 
 const HeroTitle = (props) => {
